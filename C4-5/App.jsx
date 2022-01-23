8b0cab4c6fb29f7319d76c0ae75ec47904404412
TetramG <dehaysvictor@gmail.com>
2022-01-23 15:00:07 +0100

    Chapitre 5

--- C4-5/App.jsx
+++ C4-5/App.jsx
@@ -56,25 +56,58 @@ class Incrementer extends React.Component{
 
     constructor(props){
         super(props)
-        this.timer = null
-        this.state={counter: props.initValue}
+        this.state={counter: props.initValue,timer: null}
     }
 
     componentDidMount(){
-        this.timer = window.setInterval(() => {
+        this.setState({timer: window.setInterval(() => {
             this.setState((state, props) => ({counter: this.state.counter + props.step}))
-        }, 1000)
+        }, 1000)})
     }
 
     componentWillUnmount(){
         window.clearInterval(this.timer)
     }
 
+    toggle(){
+        if(this.state.timer){
+            window.clearInterval(this.state.timer)
+            this.setState({timer: null})
+        }
+        else{
+            this.setState({timer: window.setInterval(() => {
+                this.setState((state, props) => ({counter: this.state.counter + props.step}))
+            }, 1000)})
+        }
+    }
+
     render(){
-        return <p>{this.state.counter}</p>
+        return <div>
+            {this.state.counter}
+            <button onClick={this.toggle.bind(this)}>{this.state.timer ? "Pause" : "Play"}</button>
+            </div>
     }
 }
 
+class ManualCounter extends React.Component{
+
+    constructor(props){
+        super(props)
+        this.state={counter:0}
+    }
+
+    increment(e){
+        e.preventDefault()
+        this.setState((state,props) => ({counter: ++this.state.counter}))
+    }
+
+    render(){
+        return <div>
+            Valeur: {this.state.counter}
+            <button onClick={this.increment.bind(this)}>+1</button>
+        </div>
+    }
+}
 
 
 function Home () {
@@ -82,6 +115,7 @@ function Home () {
         <Welcome name="Victor">Enfant</Welcome>
         <Clock/>
         <Incrementer initValue={10} step={1}></Incrementer>
+        <ManualCounter></ManualCounter>
     </div>
 }
 
