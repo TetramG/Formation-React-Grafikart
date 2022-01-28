4aba21c6565037057a90fd2927d6e5aca7b973eb
TetramG <dehaysvictor@gmail.com>
2022-01-28 17:07:18 +0100

    Chapitre 13 setup

---
+++ C13/tuto-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import {Clock} from './Clock.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Clock></Clock>
+    </div>
+  );
+}
+
+export default App;
